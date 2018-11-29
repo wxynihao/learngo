fe95a5410afde6940fc62285cdf711e30a83dcc6
Rain <wxynihao@gmail.com>
2018-11-30 00:54:03 +0800

    add files

---
+++ src/container/array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func arr() {
+	var arr1 [5]int
+	arr2 := [3]int{1, 3, 5}
+	arr3 := [...]int{2, 4, 6, 8, 10}
+	fmt.Println(arr1, arr2, arr3)
+}
+
+func main() {
+	arr()
+}
