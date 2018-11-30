818aef5f924402a4749c047eb91db090318d31d4
Rain <wxynihao@gmail.com>
2018-12-01 01:22:01 +0800

    内建容器部分

--- src/container/array.go
+++ src/container/array.go
@@ -6,7 +6,24 @@ func arr() {
 	var arr1 [5]int
 	arr2 := [3]int{1, 3, 5}
 	arr3 := [...]int{2, 4, 6, 8, 10}
+	var grid [4][5]int
+
 	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(grid)
+}
+
+func traversal() {
+	var arr []int
+	for i := range arr {
+		fmt.Println(i)
+	}
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+	for _, v := range arr {
+		fmt.Println(v)
+	}
+
 }
 
 func main() {
---
+++ src/container/slice.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func printSlice(s []int) {
+	fmt.Printf("%v, len=%d, cap=%d\n",
+		s, len(s), cap(s))
+}
+
+func sliceFromArr() {
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(arr[2:6])
+	fmt.Println(arr[:6])
+	fmt.Println(arr[2:])
+	fmt.Println(arr[:])
+}
+
+func arr2Slice() {
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	s1 := arr[2:]
+	updateSlice(s1)
+	fmt.Println("s1 =", s1)
+}
+
+func slice() {
+	s1 := []int{2, 4, 6, 8}
+	printSlice(s1)
+
+	s2 := make([]int, 16)
+	s3 := make([]int, 10, 32)
+	printSlice(s2)
+	printSlice(s3)
+}
+
+func slice2Slice() {
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	s1 := arr[2:6]
+	s2 := s1[3:5]
+
+	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d\n",
+		s1, len(s1), cap(s1))
+	fmt.Printf("s2=%v, len(s2)=%d, cap(s2)=%d\n",
+		s2, len(s2), cap(s2))
+}
+
+func updateSlice(s []int) {
+	s[0] = 100
+}
+
+func main() {
+
+}
